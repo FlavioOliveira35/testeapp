784c9f3b480c28a73fefc84456a1e2a56630ff6c
FLAVIO CESAR DE OLIVEIRA <91300183+FlavioOliveira35@users.noreply.github.com>
2025-05-19 20:49:41 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAlFPchcsdsyJWmOZUqfdbm-EbwkF01Awc",
+    authDomain: "controle-4b27d.firebaseapp.com",
+    projectId: "controle-4b27d",
+    storageBucket: "controle-4b27d.firebasestorage.app",
+    messagingSenderId: "194889796690",
+    appId: "1:194889796690:web:c3162d4193252454fee38b"
+  };
+  
+
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Inicializar Firestore
+const db = firebase.firestore();
+
+// Referências para as coleções
+const maquinasRef = db.collection("maquinas");
+console.log(maquinasRef);
+const operacoesRef = db.collection("operacoes");
+const pecasRef = db.collection("pecas");
+const manutencoesRef = db.collection("manutencoes");
+const alertasRef = db.collection("alertas");
