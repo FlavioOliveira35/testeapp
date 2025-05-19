44b4aa3d3a5447cb837e23dd6bf25e0d6d4ae497
FLAVIO CESAR DE OLIVEIRA <91300183+FlavioOliveira35@users.noreply.github.com>
2025-05-19 19:45:50 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+  
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +13,7 @@
     messagingSenderId: "194889796690",
     appId: "1:194889796690:web:c3162d4193252454fee38b"
   };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  
 
 
 // Inicializar Firebase
